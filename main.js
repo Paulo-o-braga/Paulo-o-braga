fcc8ecef98cd734d39607d73203d37ebe5664da5
Paulo Braga <98926055+Paulo-o-braga@users.noreply.github.com>
2022-02-16 13:53:09 -0400

    Impar ou par
    Verificar se um um número é impar ou par.

---
+++ main.js
@@ -0,0 +1,19 @@
+//comentário: Olá, Mundo
+/*Paulo de Oliveira Braga
+Licenciatura em Computação
+Matrícula: 1915280021*/
+
+var number;
+var atv;
+number = 20;
+console.log("Olá, Mundo!");
+console.log(number);
+
+
+atv =number%2;
+if(atv%2==0){
+    console.log("O Número é par");
+}else{
+    console.log("Número ímpar");
+}
+
